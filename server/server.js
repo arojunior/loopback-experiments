1576402779af03cf5c9802e3698239739b6c9fe1
Junior Oliveira <arojunior@gmail.com>
2018-03-05 11:14:55 -0300

    add GraphQL server integration

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
+const config = require('./component-config.json');
+const graphQLConfig = config['loopback-graphql-server'];
 var app = module.exports = loopback();
 
 app.start = function() {
@@ -14,6 +15,12 @@ app.start = function() {
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
+    if (graphQLConfig) {
+      const graphql = graphQLConfig.path;
+      const playground = graphQLConfig.graphiqlPath;
+      console.log('GraphQL listening at: %s%s', baseUrl, graphql);
+      console.log('Browse GraphQL IDE at: %s%s', baseUrl, playground);
     }
   });
 };
