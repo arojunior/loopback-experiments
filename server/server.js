e50ed23f0f56a2b9aa249435f57712973c05b8b8
Junior Oliveira <arojunior@gmail.com>
2018-03-05 11:21:17 -0300

    refactoring to ES6

--- server/server.js
+++ server/server.js
@@ -1,29 +1,29 @@
 'use strict';
 
-var loopback = require('loopback');
-var boot = require('loopback-boot');
+const loopback = require('loopback');
+const boot = require('loopback-boot');
 const config = require('./component-config.json');
 const graphQLConfig = config['loopback-graphql-server'];
-var app = module.exports = loopback();
+const app = loopback();
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-    if (graphQLConfig) {
-      const graphql = graphQLConfig.path;
-      const playground = graphQLConfig.graphiqlPath;
-      console.log('GraphQL listening at: %s%s', baseUrl, graphql);
-      console.log('Browse GraphQL IDE at: %s%s', baseUrl, playground);
-    }
-  });
-};
+// start the web server
+app.start = () => app.listen(function() {
+  app.emit('started');
+  const baseUrl = app.get('url').replace(/\/$/, '');
+  console.log('Web server listening at: %s', baseUrl);
+
+  if (app.get('loopback-component-explorer')) {
+    const explorerPath = app.get('loopback-component-explorer').mountPath;
+    console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+  }
+
+  if (graphQLConfig) {
+    const graphql = graphQLConfig.path;
+    const playground = graphQLConfig.graphiqlPath;
+    console.log('GraphQL listening at: %s%s', baseUrl, graphql);
+    console.log('Browse GraphQL IDE at: %s%s', baseUrl, playground);
+  }
+});
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
@@ -34,3 +34,5 @@ boot(app, __dirname, function(err) {
   if (require.main === module)
     app.start();
 });
+
+module.exports = app;
